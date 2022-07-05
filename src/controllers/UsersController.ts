48a9c507a4dbc02ab115f20de56cf08d82ccd451
Andre Castelo <andrecastelo@gmail.com>
2022-07-05 15:39:33 -0300

    fix order

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -6,7 +6,7 @@ export const UsersController = {
   createValidation: z.object({
     body: z.object({
       name: z.string().min(1).max(30),
-      email: z.string().email().min(1),
+      email: z.string().min(1).email(),
     }),
   }),
 
